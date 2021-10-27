ab76ad93f3314a66ad5d9d6b61afab445268a3b5
jonevans20 <91927957+jonevans20@users.noreply.github.com>
2021-10-27 13:31:43 -0500

    main.js updates

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+alert("hello, world")
+
+window.alert("hello, world!")
+
+document.write("hello, world. <br>")
+
+var x="Hello, World! <br>"
+document.write(x)
+
+var a="This is a string. <br> "
+document.write(a)
+
+var b="Alpha Bravo Charlie Delta "
+window.alert(b)
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell Dad!\" <br> \"Eat my shorts!\" Bart responded. <br>");
+
+document.write("Ayrton Senna once said, \"If you no longer go for a gap, you are no longer a racing driver\" <br>");
+
+document.write("Darth Vader has said, \"I find your lack of faith disturbing.\" "
++ "He has also gone on to drop other great quotes such as, \"I am altering the deal, pray I don\'t alter it any further\"<br>");
+
+var characters="Star Wars <br>", Dad="Anakin Skywalker/ Darth Vader<br>", Son="Luke Skywalker<br>", Daughter="Princess Leia<br>"
+var Dad = Dad.fontcolor("red")
+var Son = Son.fontcolor("green")
+var Daughter = Daughter.fontcolor("blue")
+document.write(Dad);
+
+
+var blues="I have the blues<br>"
+var blues= blues.fontcolor("blue");
+document.write(blues);
+
+document.write(5*5);
