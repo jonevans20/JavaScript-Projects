de0c2cdb06f194de0aa6a2b496b28201ad5367b6
jonevans20 <91927957+jonevans20@users.noreply.github.com>
2021-10-27 18:31:51 -0500

    JavaScript Practice

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "If you only knew the power"; //I split this quote in half to be able to concatenate two strings
+    sentence += " of the Dark Side!" //One of the greatest quotes... ever...    
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
