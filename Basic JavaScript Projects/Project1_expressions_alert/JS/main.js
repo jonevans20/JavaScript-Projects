6fb8a30b3ad0c28efc1ce375c1440ec298e7e49f
jonevans20 <91927957+jonevans20@users.noreply.github.com>
2021-10-27 14:22:00 -0500

    updated main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,34 +1,35 @@
-alert("hello, world")
+alert("hello, world") //Loads a window that says "hello, world" with an okay button
 
-window.alert("hello, world!")
+window.alert("hello, world!") //Also opens a windo that says "hello, world" with an okay button
 
-document.write("hello, world. <br>")
+document.write("hello, world. <br>") //writes hello, world on the main page. Has a break after to write the following code on the next line
 
-var x="Hello, World! <br>"
+var x="Hello, World! <br>" //assigns hello, world to a variable
 document.write(x)
 
-var a="This is a string. <br> "
+var a="This is a string. <br> " //assigns a string to a variable
 document.write(a)
 
-var b="Alpha Bravo Charlie Delta "
-window.alert(b)
+var b="Alpha Bravo Charlie Delta " //assigns a variable to a window that loads when the page is loaded
+window.alert(b) 
 
-document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell Dad!\" <br> \"Eat my shorts!\" Bart responded. <br>");
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell Dad!\" <br> \"Eat my shorts!\" Bart responded. <br>"); //practice to use properly use quotations in the code
 
-document.write("Ayrton Senna once said, \"If you no longer go for a gap, you are no longer a racing driver\" <br>");
+document.write("Ayrton Senna once said, \"If you no longer go for a gap, you are no longer a racing driver\" <br>"); //practice to use quotations in the code
 
 document.write("Darth Vader has said, \"I find your lack of faith disturbing.\" "
-+ "He has also gone on to drop other great quotes such as, \"I am altering the deal, pray I don\'t alter it any further\"<br>");
++ "He has also gone on to drop other great quotes such as, \"I am altering the deal, pray I don\'t alter it any further\"<br>"); //concatonating practice
 
-var characters="Star Wars <br>", Dad="Anakin Skywalker/ Darth Vader<br>", Son="Luke Skywalker<br>", Daughter="Princess Leia<br>"
+var characters="Star Wars <br>", Dad="Anakin Skywalker/ Darth Vader <br>", Son="Luke Skywalker<br>", Daughter="Princess Leia<br>" //practice on assigning multiple variables and assigning colors to those variables
 var Dad = Dad.fontcolor("red")
 var Son = Son.fontcolor("green")
 var Daughter = Daughter.fontcolor("blue")
-document.write(Dad);
+document.write(Dad + Son); //utilized the two variables of Dad and Son and concatonated them together when calling the code.
 
 
-var blues="I have the blues<br>"
+var blues="I have the blues<br>" //practice assigning colors to variables
 var blues= blues.fontcolor("blue");
 document.write(blues);
 
-document.write(5*5);
+document.write(5*5); //practice with expressions
+
