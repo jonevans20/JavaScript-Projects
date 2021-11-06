5f1d37031e45c0912a517bf5af6c4ff8b4b6d620
jonevans20 <91927957+jonevans20@users.noreply.github.com>
2021-11-05 23:28:23 -0500

    One Page Website assignment

--- One-Page Website/JS/one_page_website.js
+++ One-Page Website/JS/one_page_website.js
@@ -1,39 +1,21 @@
-//Open the Modal
-function openModal() {
-    document.getElementById("myModal").style.display + "block";
-}
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
 
-//Close the Modal
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
+const images = document.querySelectorAll('img')
+images.forEach(image=> {
+    image.addEventListener('click',e=> {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while(lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-//Next/Previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-//Thumbnail image controls 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n < 1) {slideIndex = slides.length}
-    for(i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i <dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML=dots[slideIndex-1].alt;
-}
+lightbox.addEventListener('click', e=> {
+    if(e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
